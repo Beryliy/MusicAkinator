94848c664c37f0d5bfd6e94038accee13d7d7fa8
Kolia Huk <urenad53@ukr.net>
2020-01-19 17:09:15 +0200

    service for auddIo

--- app/src/main/java/com/fourcore/musicakinator/network/FindLyricsService.kt
+++ app/src/main/java/com/fourcore/musicakinator/network/FindLyricsService.kt
@@ -1,4 +1,10 @@
 package com.fourcore.musicakinator.network
 
-interface LyricRecogniseService {
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface FindLyricsService {
+    @GET("/findLyrics")
+    fun findLyrics(@Query("q") lyrics: String): Observable
 }
