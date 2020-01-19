6f8c561818c7db1e872656e4fbb7a0465ff5c2dc
Kolia Huk <urenad53@ukr.net>
2020-01-19 10:14:51 +0200

    make AuddIo Retrofit compatible with RxJava

--- app/src/main/java/com/fourcore/musicakinator/di/module/NetworkModule.kt
+++ app/src/main/java/com/fourcore/musicakinator/di/module/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
@@ -13,15 +14,24 @@ object NetworkModule {
     @Provides
     @Singleton
     @JvmStatic
-    fun provideOkhttp(): OkHttpClient = OkHttpClient.Builder().build()
+    fun provideOkhttp() = OkHttpClient.Builder().build()
 
     @Provides
     @Singleton
     @JvmStatic
-    fun provideAuddIoRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    fun provideRxJava2CallAdapterFactory() = RxJava2CallAdapterFactory.create()
+
+    @Provides
+    @Singleton
+    @JvmStatic
+    fun provideAuddIoRetrofit(
+        okHttpClient: OkHttpClient,
+        rxJava2CallAdapterFactory: RxJava2CallAdapterFactory
+    ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.AUDDIO_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(rxJava2CallAdapterFactory)
             .client(okHttpClient)
             .build()
     }
