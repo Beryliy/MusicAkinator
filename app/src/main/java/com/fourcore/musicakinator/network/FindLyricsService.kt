e6f1b85f2a1e67dbdba7d77208e090c428354f64
Kolia Huk <urenad53@ukr.net>
2020-01-20 00:36:51 +0200

    remove rxjava

--- app/src/main/java/com/fourcore/musicakinator/network/FindLyricsService.kt
+++ app/src/main/java/com/fourcore/musicakinator/network/FindLyricsService.kt
@@ -1,10 +1,9 @@
 package com.fourcore.musicakinator.network
 
-import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface FindLyricsService {
     @GET("/findLyrics")
-    fun findLyrics(@Query("q") lyrics: String): Observable
+    fun findLyrics(@Query("q") lyrics: String)
 }
