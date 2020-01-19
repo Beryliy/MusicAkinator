1bfebb08a49eeee563f148940b2d141216af3a51
Kolia Huk <urenad53@ukr.net>
2020-01-19 09:44:21 +0200

    dagger component for application

--- app/src/main/java/com/fourcore/musicakinator/di/AppComponent.kt
+++ app/src/main/java/com/fourcore/musicakinator/di/AppComponent.kt
@@ -1,4 +1,23 @@
 package com.fourcore.musicakinator.di
 
-interface AppComponent {
+import android.app.Application
+import com.fourcore.musicakinator.MusicAkinatorApp
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+import javax.inject.Singleton
+
+@Singleton
+@Component
+interface AppComponent: AndroidInjector<DaggerApplication> {
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun application(application: Application): Builder
+        fun build(): AppComponent
+    }
+
+    fun inject(application: MusicAkinatorApp)
 }
