87e8b55c777c4bfdc4715234713b586fca4eafbb
radikushch <radikushch@gmail.com>
2020-01-20 12:33:53 +0200

    feat: update gradle plugin 3.5.2 -> 3.5.3

--- app/src/main/java/com/fourcore/musicakinator/MusicAkinatorApp.kt
+++ app/src/main/java/com/fourcore/musicakinator/MusicAkinatorApp.kt
@@ -1,8 +1,6 @@
 package com.fourcore.musicakinator
 
 import com.fourcore.musicakinator.di.component.DaggerAppComponent
-import dagger.android.AndroidInjector
-import dagger.android.DaggerApplication
 
 class MusicAkinatorApp: DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
