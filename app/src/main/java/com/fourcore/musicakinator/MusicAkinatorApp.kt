565da1f8f984dc43606e9eb1186e6cad3d4b40ca
radikushch <radikushch@gmail.com>
2020-01-20 14:08:44 +0200

    feat: add dependencies

--- app/src/main/java/com/fourcore/musicakinator/MusicAkinatorApp.kt
+++ app/src/main/java/com/fourcore/musicakinator/MusicAkinatorApp.kt
@@ -1,6 +1,8 @@
 package com.fourcore.musicakinator
 
 import com.fourcore.musicakinator.di.component.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
 
 class MusicAkinatorApp: DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
