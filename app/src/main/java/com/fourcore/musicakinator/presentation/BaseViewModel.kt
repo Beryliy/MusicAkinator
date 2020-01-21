655d14489b3c0d4b63e5962f0f96fae2586939db
Kolia Huk <urenad53@ukr.net>
2020-01-21 16:47:11 +0200

    implement Coroutine scope

--- app/src/main/java/com/fourcore/musicakinator/presentation/BaseViewModel.kt
+++ app/src/main/java/com/fourcore/musicakinator/presentation/BaseViewModel.kt
@@ -1,6 +1,18 @@
 package com.fourcore.musicakinator.presentation
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancelChildren
+import kotlin.coroutines.CoroutineContext
 
-open class BaseViewModel: ViewModel() {
+open class BaseViewModel: ViewModel(), CoroutineScope {
+    private val job = SupervisorJob()
+    override val coroutineContext: CoroutineContext = Dispatchers.IO + job
+
+    override fun onCleared() {
+        job.cancelChildren()
+        super.onCleared()
+    }
 }
