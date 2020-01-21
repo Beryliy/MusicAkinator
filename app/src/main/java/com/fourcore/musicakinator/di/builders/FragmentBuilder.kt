f34d9e4c67037a7ce028bef15c72457be4f3879f
Kolia Huk <urenad53@ukr.net>
2020-01-21 14:13:40 +0200

    dagger module for LyricRecogniserFragment that provides databinding object for ViewModel

--- app/src/main/java/com/fourcore/musicakinator/di/builders/FragmentBuilder.kt
+++ app/src/main/java/com/fourcore/musicakinator/di/builders/FragmentBuilder.kt
@@ -2,13 +2,17 @@ package com.fourcore.musicakinator.di.builders
 
 import com.fourcore.musicakinator.di.FragmentScope
 import com.fourcore.musicakinator.di.module.lyricRecogniser.LyricRecogniserBindsModule
+import com.fourcore.musicakinator.di.module.lyricRecogniser.LyricRecogniserProvidesModule
 import com.fourcore.musicakinator.presentation.lyricRecogniser.LyricRecogniserFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class FragmentBuilder {
-    @ContributesAndroidInjector(modules = [LyricRecogniserBindsModule::class])
+    @ContributesAndroidInjector(modules = [
+        LyricRecogniserBindsModule::class,
+        LyricRecogniserProvidesModule::class
+    ])
     @FragmentScope
     abstract fun contributeLyricRecogniserFragment(): LyricRecogniserFragment
 }
