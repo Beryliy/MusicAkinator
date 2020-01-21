1b19729d03cc083da3bf6c846f285b76cd097724
Kolia Huk <urenad53@ukr.net>
2020-01-21 16:46:27 +0200

    math layout with fields in databinding class

--- app/src/main/java/com/fourcore/musicakinator/presentation/lyricRecogniser/LyricRecogniserData.kt
+++ app/src/main/java/com/fourcore/musicakinator/presentation/lyricRecogniser/LyricRecogniserData.kt
@@ -1,9 +1,17 @@
 package com.fourcore.musicakinator.presentation.lyricRecogniser
 
 import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import androidx.databinding.library.baseAdapters.BR
 import com.fourcore.musicakinator.di.FragmentScope
 import javax.inject.Inject
 
 @FragmentScope
 class LyricRecogniserData @Inject constructor(): BaseObservable() {
+    @get:Bindable
+    var lyrics = String()
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.lyrics)
+        }
 }
