04065562671e844969513275a31c2a2f2d4219b5
Kolia Huk <urenad53@ukr.net>
2020-01-21 11:45:38 +0200

    base ViewModel

--- app/src/main/java/com/fourcore/musicakinator/presentation/BaseViewModel.java
+++
@@ -1,6 +0,0 @@
-package com.fourcore.musicakinator.presentation;
-
-import androidx.lifecycle.ViewModel;
-
-public class BaseViewModel extends ViewModel {
-}
---
+++ app/src/main/java/com/fourcore/musicakinator/presentation/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.fourcore.musicakinator.presentation
+
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel: ViewModel() {
+}
