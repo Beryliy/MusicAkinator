730c4d549864c2da5e6ac8b340041c0c1915724f
Kolia Huk <urenad53@ukr.net>
2020-01-24 00:26:45 +0200

    bind SharedViewModel for in dagger module

--- app/src/main/java/com/fourcore/musicakinator/di/builders/ActivityBuilder.kt
+++ app/src/main/java/com/fourcore/musicakinator/di/builders/ActivityBuilder.kt
@@ -1,5 +1,6 @@
 package com.fourcore.musicakinator.di.builders
 
+import com.fourcore.musicakinator.di.module.MainActivityBindsModule
 import com.fourcore.musicakinator.presentation.MainActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -7,6 +8,8 @@ import dagger.android.ContributesAndroidInjector
 @Module
 abstract class ActivityBuilder {
 
-    @ContributesAndroidInjector
+    @ContributesAndroidInjector(modules = [
+        MainActivityBindsModule::class
+    ])
     abstract fun contributeStartActivity(): MainActivity
 }
---
+++ app/src/main/java/com/fourcore/musicakinator/di/module/MainActivityBindsModule.kt
@@ -0,0 +1,16 @@
+package com.fourcore.musicakinator.di.module
+
+import androidx.lifecycle.ViewModel
+import com.fourcore.musicakinator.di.factory.ViewModelKey
+import com.fourcore.musicakinator.presentation.game.GameViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+interface MainActivityBindsModule {
+    @Binds
+    @IntoMap
+    @ViewModelKey(GameViewModel::class)
+    fun bindGameViewModel(lyricsRecogniserViewModel: GameViewModel): ViewModel
+}
