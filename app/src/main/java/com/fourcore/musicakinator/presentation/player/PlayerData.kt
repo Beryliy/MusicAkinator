f43e3e8b319fe2bb05f3f762e4740267e942f1e6
Kolia Huk <urenad53@ukr.net>
2020-01-23 21:25:44 +0200

    declare title, artist fields in databinding

--- app/src/main/java/com/fourcore/musicakinator/presentation/player/PlayerData.kt
+++ app/src/main/java/com/fourcore/musicakinator/presentation/player/PlayerData.kt
@@ -1,9 +1,24 @@
 package com.fourcore.musicakinator.presentation.player
 
 import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import androidx.databinding.library.baseAdapters.BR
 import com.fourcore.musicakinator.di.FragmentScope
 import javax.inject.Inject
 
 @FragmentScope
 class PlayerData @Inject constructor(): BaseObservable() {
+    @get:Bindable
+    var trackTitle = ""
+    set(value) {
+        field = value
+        notifyPropertyChanged(BR.trackTitle)
+    }
+
+    @get:Bindable
+    var artist = ""
+    set(value) {
+        field = value
+        notifyPropertyChanged(BR.artist)
+    }
 }
