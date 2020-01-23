6b58ed6d2e56d39522d54bf672949dd8a8ff2d44
Kolia Huk <urenad53@ukr.net>
2020-01-23 14:25:56 +0200

    setup databinding for player fragment

--- app/src/main/java/com/fourcore/musicakinator/presentation/player/PlayerFragment.kt
+++ app/src/main/java/com/fourcore/musicakinator/presentation/player/PlayerFragment.kt
@@ -1,26 +1,42 @@
 package com.fourcore.musicakinator.presentation.player
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.navArgs
 
 import com.fourcore.musicakinator.R
+import com.fourcore.musicakinator.databinding.FragmentPlayerBinding
+import dagger.android.support.AndroidSupportInjection
+import javax.inject.Inject
 
-/**
- * A simple [Fragment] subclass.
- */
 class PlayerFragment : Fragment() {
+    val args: PlayerFragmentArgs by navArgs()
+    @Inject lateinit var viewModel: PlayerViewModel
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player, container, false)
+        val databinding = DataBindingUtil.inflate<FragmentPlayerBinding>(
+            inflater,
+            R.layout.fragment_player,
+            container,
+            false
+        )
+        databinding.viewModel = viewModel
+        databinding.playerData = viewModel.playerData
+        return databinding.root
     }
-
-
 }
