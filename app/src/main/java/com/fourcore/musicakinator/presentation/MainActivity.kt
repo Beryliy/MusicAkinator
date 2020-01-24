f53a228818ba51e24e94cbffc79c1df5a4cbd3b3
Kolia Huk <urenad53@ukr.net>
2020-01-24 03:22:52 +0200

    provide dependencies for fragments

--- app/src/main/java/com/fourcore/musicakinator/presentation/MainActivity.kt
+++ app/src/main/java/com/fourcore/musicakinator/presentation/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.fourcore.musicakinator.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 import com.fourcore.musicakinator.R
 import dagger.android.AndroidInjection
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity(), HasAndroidInjector {
+class MainActivity : FragmentActivity(), HasAndroidInjector {
     @Inject lateinit var androidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate(savedInstanceState: Bundle?) {
