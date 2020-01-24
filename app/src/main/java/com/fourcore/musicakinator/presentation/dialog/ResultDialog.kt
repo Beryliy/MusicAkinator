30d1e839f749145d0b1be53818a756371369c9e3
Kolia Huk <urenad53@ukr.net>
2020-01-24 22:45:20 +0200

    change dialog style

--- app/src/main/java/com/fourcore/musicakinator/presentation/dialog/ResultDialog.kt
+++ app/src/main/java/com/fourcore/musicakinator/presentation/dialog/ResultDialog.kt
@@ -13,7 +13,7 @@ class ResultDialog(
     @StringRes val description: Int
 ) {
     fun show(activity: Activity) {
-        val builder = AlertDialog.Builder(activity)
+        val builder = AlertDialog.Builder(activity, R.style.ResultDialog)
         val view = activity.layoutInflater.inflate(R.layout.dialog_result, null)
         view.findViewById<ImageView>(R.id.resultIv).setImageResource(image)
         view.findViewById<TextView>(R.id.resultTv).setText(description)
