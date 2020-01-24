97d14539b0d7602ae85b90185fb0999f67521340
Kolia Huk <urenad53@ukr.net>
2020-01-24 02:52:23 +0200

    dialog for showing game result

---
+++ app/src/main/java/com/fourcore/musicakinator/presentation/dialog/ResultDialog.kt
@@ -0,0 +1,22 @@
+package com.fourcore.musicakinator.presentation.dialog
+
+import android.app.Activity
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
+import com.fourcore.musicakinator.R
+
+class ResultDialog(
+    @DrawableRes val image: Int,
+    @StringRes val description: Int
+) {
+    fun show(activity: Activity) {
+        val builder = AlertDialog.Builder(activity)
+        val view = activity.layoutInflater.inflate(R.layout.dialog_result, null)
+        view.findViewById<ImageView>(R.id.resultIv).setImageResource(image)
+        view.findViewById<TextView>(R.id.resultTv).setText(description)
+        builder.setView(view).setCancelable(true).show()
+    }
+}
